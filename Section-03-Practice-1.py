2f8f1490d9f1a51561974bb1cc472812c6953d10
LeeSungIlLee <huntersung@yahoo.com>
2022-03-23 18:03:41 +0900

    03-22-2022

---
+++ Section-03-Practice-1.py
@@ -0,0 +1,12 @@
+boolVar, intVar, floatVar, strVar = True, 0, 0.0, ""
+
+## type(boolVar), type(intVar), type(floatVar), type(strVar) ##
+
+boolVar, intVar, floatVar, strVar = False, 100, 123.45, "안녕?"
+var1 = 2
+var1 = var1 + 200
+print(var1 + 600)
+
+0b10010011; int('10010011', 2)
+
+0x93; int('93', 16)
